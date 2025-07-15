c678e48825d15575f08e82b50894ae5c8cdd86e6
yss120612 <yss1@yandex.ru>
2025-07-15 15:37:07 +0800

    150725-1

--- lib7/src/main/java/com/yss1/lib7/JmeFragmentBase.java
+++ lib7/src/main/java/com/yss1/lib7/JmeFragmentBase.java
@@ -11,6 +11,8 @@ import android.content.Context;
 import android.content.SharedPreferences.Editor;
 import android.content.res.Resources;
 import android.content.Intent;
+import android.net.Network;
+import android.net.NetworkCapabilities;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -48,7 +50,72 @@ import java.util.logging.Logger;
 
 import static android.app.Activity.RESULT_OK;
 
+/*
+public interface AndroidIF  {
 
+    //сохранение параметров
+    public void saveSettingInt(String k,int v);
+    public int  loadSettingInt(String k,int dv);
+    public void saveSettingLong(String k,long v);
+    public long  loadSettingLong(String k,long dv);
+    public void saveSettingBool(String k,boolean v);
+    public boolean  loadSettingBool(String k,boolean dv);
+    public void saveSettingStr(String k,String v);
+    public String  loadSettingStr(String k,String dv);
+    public void saveSettingStrSet(String k,Set<String> v);
+    public Set<String>  loadSettingStrSet(String k);
+    public void removeStrings(ArrayList<String> al);
+    public void saveSettingsMap(Map<String,Object> M);
+    public Map<String,?> readAll();
+    //конец сохранения параметров
+
+    public void displayInterstitial();
+    public boolean isInterstitialLoaded();
+    public void loadAd();
+    public void showBanner(boolean sb);
+    public boolean isBannerVisible();
+
+    //public void setIds();
+    public String  getLanguage();
+    public void setScreenOFF(boolean off);
+    public void writeLog(String theme);
+    public void getInput(int dt,String tit, String sinit);
+    public void showMessage(String t, String m);
+    public void showInfo(String t);
+    public void rate(String what);
+    public void sendEventGA(String app,String s);
+    //public String getName();
+    public void mp_signIn();
+    public void mp_signOut(boolean signout_google);
+    public boolean mp_isSignedIn();
+    public void mp_writeDB(String field, Map<String,Object> value);
+    public void mp_readDB(String field);
+    public void mp_connectDataReceiverFor(String field);
+
+    //public boolean gp_isSignedIn();
+    //public boolean mp_connect();
+    //public void mp_disconnect();
+    //public void gp_ShowAchivements();
+    //public void gp_ShowLeaderboard(String id);
+    //public void gp_SubmitScore(String id, int sc);
+    //public void gp_UnlockAchivement(String Aid);
+    //public void gp_IncrementAchivement(String Aid,int sc);
+
+    //public void mp_SelectOpponents(boolean allowAutomatch);
+    //public void mp_QuickStartGame();
+    //public void mp_StartWaitingRoom();
+    //public void mp_InvitationInbox();
+    //public int  mp_MaxMessageLength();
+//  public void mp_Send2All(byte [] data);
+//  public void mp_Send2One(String id, byte [] data);
+//  public void mp_Sendll(byte [] data);
+    //public void mp_ReSendTo(String id, byte [] data);
+    //public void mp_SetSendCounter(int val);
+    //public int mp_SendTo(String id, byte [] data);
+    //public void mp_endNetworkGame();
+
+}
+*/
 public class JmeFragmentBase extends AndroidHarness implements
         AndroidIF
 //        GoogleApiClient.ConnectionCallbacks,
@@ -78,9 +145,7 @@ public class JmeFragmentBase extends AndroidHarness implements
     protected DatabaseReference mDatabase;
     protected long startTime;
     protected FirebaseAnalytics mFirebaseAnalytics;
-    //protected static GoogleAnalytics analytics;
-    //protected ActivityGoogleBinding mBinding;
-    //private lateinit var mBinding: ResultProfileBinding
+
 
     //    protected Room  mRoom;
     protected String mIncomingInvitationId;
@@ -94,8 +159,6 @@ public class JmeFragmentBase extends AndroidHarness implements
 
 
     protected boolean trakerSent;
-    //    protected static boolean GP_ACTIVATE;
-//    protected static boolean GP_MULTIPLAYER;
     protected boolean AD_BANNER;
 
     private boolean off_screen;
@@ -189,7 +252,7 @@ public class JmeFragmentBase extends AndroidHarness implements
                     Editor Ed = mSettings.edit();
                     Ed.clear();
                     Ed.putInt("VERSION", versionNumber);
-                    Ed.commit();
+                    Ed.apply();
                     jmeapp.afterClearOnWrongVersion();
                 }
             }
@@ -209,19 +272,26 @@ public class JmeFragmentBase extends AndroidHarness implements
         boolean haveConnectedMobile = false;
 
         ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo[] networkInfo = cm.getAllNetworkInfo();
-        for (NetworkInfo nw : networkInfo) {
-            if (nw.getTypeName().equalsIgnoreCase("WIFI")) {
-                if (nw.isConnected()) {
-                    haveConnectedWifi = true;
-                }
-            }
-            if (nw.getTypeName().equalsIgnoreCase("MOBILE")) {
-                if (nw.isConnected()) {
-                    haveConnectedMobile = true;
-                }
-            }
+        Network network = cm.getActiveNetwork();
+        NetworkCapabilities networkCapabilities=cm.getNetworkCapabilities(network);
+        if (networkCapabilities!=null){
+            haveConnectedMobile =networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR);
+            haveConnectedWifi =networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI);
         }
+
+//        NetworkInfo[] networkInfo = cm.getAllNetworkInfo();
+//        for (NetworkInfo nw : networkInfo) {
+//            if (nw.getTypeName().equalsIgnoreCase("WIFI")) {
+//                if (nw.isConnected()) {
+//                    haveConnectedWifi = true;
+//                }
+//            }
+//            if (nw.getTypeName().equalsIgnoreCase("MOBILE")) {
+//                if (nw.isConnected()) {
+//                    haveConnectedMobile = true;
+//                }
+//            }
+//        }
         return haveConnectedWifi || haveConnectedMobile;
     }
 
@@ -315,21 +385,16 @@ public class JmeFragmentBase extends AndroidHarness implements
     public void mp_writeDB(String field, Map<String, Object> value) {
         if (mDatabase == null) return;
         mDatabase.child(field).setValue(value);
-        //mDatabase.child(field).setValue(value);
-        //Task<Void> task=myRef.setValue(value);
-        //task.
     }
 
     public void mp_readDB(String field) {
         if (mDatabase == null) return;
-
         mDatabase.child(field).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DataSnapshot> task) {
                 if (!task.isSuccessful()) {
                     //showInfo("ERROR");
                     Log.e("firebase", "Error getting data", task.getException());
-
                 } else {
                     if (task.getResult() == null || task.getResult().getValue() == null) {
                         jmeapp.recvData(field, null);
@@ -630,7 +695,7 @@ public class JmeFragmentBase extends AndroidHarness implements
             for (String SR : al) {
                 Ed.remove(SR);
             }
-            Ed.commit();
+            Ed.apply();
         } catch (Exception E) {
             Log.d("Yss", "Error in [removeStrings] " + E.getMessage());
         }
@@ -653,7 +718,7 @@ public class JmeFragmentBase extends AndroidHarness implements
                     Ed.putString(entry.getKey().toString(), entry.getValue().toString());
                 }
             }
-            Ed.commit();
+            Ed.apply();
             // writeLog("Saved objects "+M.size());
         } catch (Exception E) {
             Log.d("Yss", "Error in [saveSettingInt] " + E.getMessage());
@@ -665,7 +730,7 @@ public class JmeFragmentBase extends AndroidHarness implements
         try {
             Editor Ed = mSettings.edit();
             Ed.putStringSet(key, value);
-            Ed.commit();
+            Ed.apply();
         } catch (Exception E) {
             Log.d("Yss", "Error in [saveSettingInt] " + E.getMessage());
         }
@@ -681,7 +746,7 @@ public class JmeFragmentBase extends AndroidHarness implements
         try {
             Editor Ed = mSettings.edit();
             Ed.putString(key, value);
-            Ed.commit();
+            Ed.apply();
         } catch (Exception E) {
             Log.d("Yss", "Error in [saveSettingInt] " + E.getMessage());
         }
@@ -698,7 +763,7 @@ public class JmeFragmentBase extends AndroidHarness implements
         try {
             Editor Ed = mSettings.edit();
             Ed.putInt(key, value);
-            Ed.commit();
+            Ed.apply();
             //Log.d("Yss", "Saved "+mSettings.getInt(key, 1));
         } catch (Exception E) {
             Log.d("Yss", "Error in [saveSettingInt] " + E.getMessage());
@@ -715,7 +780,7 @@ public class JmeFragmentBase extends AndroidHarness implements
         try {
             Editor Ed = mSettings.edit();
             Ed.putLong(key, value);
-            Ed.commit();
+            Ed.apply();
         } catch (Exception E) {
             Log.d("Yss", "Error in [saveSettingInt] " + E.getMessage());
         }
@@ -731,7 +796,7 @@ public class JmeFragmentBase extends AndroidHarness implements
         try {
             Editor Ed = mSettings.edit();
             Ed.putBoolean(key, value);
-            Ed.commit();
+            Ed.apply();
         } catch (Exception E) {
             Log.d("Yss", "Error in [saveSettingInt] " + E.getMessage());
         }
@@ -748,6 +813,7 @@ public class JmeFragmentBase extends AndroidHarness implements
     public void sendEventGA(String app, String s) {
         Bundle bundle = new Bundle();
         bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, s);
+        
         bundle.putString(FirebaseAnalytics.Param.CONTENT, app + "_" + s);
         mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.LEVEL_START, bundle);
     }
@@ -764,7 +830,6 @@ public class JmeFragmentBase extends AndroidHarness implements
 
     @Override
     public void showMessage(String tit, String msg) {
-
         new MessageDialog().init(tit, msg, this).show(getFragmentManager(), "msg");
     }
 
